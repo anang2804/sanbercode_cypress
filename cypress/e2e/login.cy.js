8d0dc36cb4eb798287ec1007c7926ab7e6041752
Anang <anang2804@users.noreply.github.com>
2026-09-22 08:19:18 +0700

    feat: add cypress login automation - 12 test case (POM)

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,94 @@
+import loginPage from "../support/loginPage";
+import loginData from "../fixtures/loginData.json";
+
+describe("OrangeHRM - Fitur Login", () => {
+  beforeEach(() => {
+    loginPage.visitUrl();
+  });
+
+  // TS-LGN-001
+  it("TS-LGN-001 - Menampilkan halaman Login dengan elemen lengkap", () => {
+    loginPage.usernameFieldVisible();
+    loginPage.passwordFieldVisible();
+    cy.get('button[type="submit"]')
+      .should("be.visible")
+      .and("contain.text", "Login");
+  });
+
+  // TS-LGN-002
+  it("TS-LGN-002 - Menampilkan field Username", () => {
+    loginPage.usernameFieldVisible();
+  });
+
+  // TS-LGN-003
+  it("TS-LGN-003 - Menampilkan field Password", () => {
+    loginPage.passwordFieldVisible();
+    loginPage.passwordFieldValidation();
+  });
+
+  // TS-LGN-004
+  it("TS-LGN-004 - Menampilkan link Forgot Password", () => {
+    cy.contains("Forgot your password?").should("be.visible");
+  });
+
+  // TS-LGN-005
+  it("TS-LGN-005 - Login berhasil dengan username dan password valid", () => {
+    loginPage.inputUsername(loginData.validUsername);
+    loginPage.inputPassword(loginData.validPassword);
+    loginPage.clickLoginBtn();
+    loginPage.dashboardUrlValidation();
+    loginPage.userDropdownNameVisible();
+  });
+
+  // TS-LGN-006
+  it("TS-LGN-006 - Login gagal dengan password salah", () => {
+    loginPage.inputUsername(loginData.validUsername);
+    loginPage.inputPassword(loginData.wrongPassword);
+    loginPage.clickLoginBtn();
+    loginPage.loginAlertValidation("Invalid credentials");
+    loginPage.loginUrlValidation();
+  });
+
+  // TS-LGN-007
+  it("TS-LGN-007 - Login gagal dengan username salah", () => {
+    loginPage.inputUsername(loginData.invalidUsername);
+    loginPage.inputPassword(loginData.validPassword);
+    loginPage.clickLoginBtn();
+    loginPage.loginAlertValidation("Invalid credentials");
+    loginPage.loginUrlValidation();
+  });
+
+  // TS-LGN-009
+  it("TS-LGN-009 - Login gagal saat username dikosongkan", () => {
+    loginPage.inputPassword(loginData.validPassword);
+    loginPage.clickLoginBtn();
+    loginPage.requiredFieldValidation("Required");
+  });
+
+  // TS-LGN-010
+  it("TS-LGN-010 - Login gagal saat password dikosongkan", () => {
+    loginPage.inputUsername(loginData.validUsername);
+    loginPage.clickLoginBtn();
+    loginPage.requiredFieldValidation("Required");
+  });
+
+  // TS-LGN-011
+  it("TS-LGN-011 - Login gagal saat username dan password dikosongkan", () => {
+    loginPage.clickLoginBtn();
+    loginPage.bothAlert();
+  });
+
+  // TS-LGN-012
+  it("TS-LGN-012 - Login berhasil menggunakan tombol Enter", () => {
+    loginPage.inputUsername(loginData.validUsername);
+    cy.get('[name="password"]').type(`${loginData.validPassword}{enter}`);
+    loginPage.dashboardUrlValidation();
+  });
+
+  // TS-LGN-013
+  it("TS-LGN-013 - Berhasil mengakses halaman Forgot Password", () => {
+    loginPage.clickForgotPassword();
+    loginPage.forgotPassUrlValidation();
+    loginPage.resetPasswordBtnVisible();
+  });
+});
---
+++ cypress/support/loginPage.js
@@ -0,0 +1,143 @@
+import loginData from "../fixtures/loginData.json";
+
+class loginPage {
+  // Navigasi
+  visitUrl() {
+    cy.visit(
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login",
+    );
+  }
+  loginUrlValidation() {
+    cy.url().should("include", "/auth/login");
+  }
+  dashboardUrlValidation() {
+    cy.url().should("include", "/dashboard/index");
+  }
+  forgotPassUrlValidation() {
+    cy.url().should("include", "/auth/requestPasswordResetCode");
+  }
+
+  // Username
+  inputUsername(username) {
+    cy.get('[name="username"]').type(username);
+  }
+  usernameFieldVisible() {
+    cy.get('[name="username"]').should("be.visible");
+  }
+
+  // Password
+  inputPassword(password) {
+    cy.get('[name="password"]').type(password);
+  }
+  passwordFieldVisible() {
+    cy.get('[name="password"]').should("be.visible");
+  }
+  passwordFieldValidation() {
+    cy.get('[name="password"]').should("have.attr", "type", "password");
+  }
+
+  // Buttons
+  clickLoginBtn() {
+    cy.get('button[type="submit"]').click();
+  }
+  clickForgotPassword() {
+    cy.contains("Forgot your password?").click();
+  }
+  clickResetPasswordBtn() {
+    cy.contains("button", "Reset Password").click();
+  }
+  clickCancelResetPasswordBtn() {
+    cy.contains("button", "Cancel").click();
+  }
+  resetPasswordBtnVisible() {
+    cy.contains("button", "Cancel").should("be.visible");
+    cy.contains("button", "Reset Password").should("be.visible");
+  }
+
+  // Validation / Alert
+  loginAlertValidation(message) {
+    cy.get(".oxd-alert-content-text").should("contain", message);
+  }
+  requiredFieldValidation(message) {
+    cy.get(".oxd-input-field-error-message").should("contain", message);
+  }
+  bothAlert() {
+    cy.get(".oxd-input-field-error-message").should("have.length", 2);
+    cy.get(".oxd-input-field-error-message").each(($el) => {
+      cy.wrap($el).should("contain", "Required");
+    });
+  }
+  userDropdownNameVisible() {
+    cy.get(".oxd-userdropdown-name").should("be.visible");
+  }
+
+  // Login Action
+  login() {
+    this.visitUrl();
+    this.inputUsername(loginData.validUsername);
+    this.inputPassword(loginData.validPassword);
+    this.clickLoginBtn();
+    this.dashboardUrlValidation();
+  }
+
+  // Intercept
+  interceptTimeAtWork() {
+    cy.intercept("GET", "**/dashboard/employees/time-at-work**").as(
+      "workingTime",
+    );
+  }
+  interceptActionSummary() {
+    cy.intercept("GET", "**/dashboard/employees/action-summary").as(
+      "actionSummary",
+    );
+  }
+  interceptShortcuts() {
+    cy.intercept("GET", "**/dashboard/shortcuts").as("shortcuts");
+  }
+  interceptFeed() {
+    cy.intercept("GET", "**/buzz/feed**").as("feed");
+  }
+  interceptLoginPageDocument() {
+    cy.intercept({
+      url: "**/auth/login",
+      resourceType: "document",
+    }).as("loginPageDoc");
+  }
+  interceptSubunit() {
+    cy.intercept("GET", "**/dashboard/employees/subunit").as("subunit");
+  }
+  interceptLocations() {
+    cy.intercept("GET", "**/dashboard/employees/locations").as("locations");
+  }
+  interceptLeaves() {
+    cy.intercept("GET", "**/dashboard/employees/leaves**").as("leaves");
+  }
+
+  // Intercept Wait Method
+  waitWorkingTime() {
+    cy.wait("@workingTime").its("response.statusCode").should("eq", 200);
+  }
+  verifyActionSummaryNotCalled() {
+    cy.get("@actionSummary.all").should("have.length", 0);
+  }
+  verifyShortcutsNotCalled() {
+    cy.get("@shortcuts.all").should("have.length", 0);
+  }
+  verifyFeedNotCalled() {
+    cy.get("@feed.all").should("have.length", 0);
+  }
+  waitLoginPageDocument() {
+    cy.wait("@loginPageDoc").its("response.statusCode").should("eq", 200);
+  }
+  verifySubunitNotCalled() {
+    cy.get("@subunit.all").should("have.length", 0);
+  }
+  waitLocations() {
+    cy.wait("@locations").its("response.statusCode").should("eq", 200);
+  }
+  waitLeaves() {
+    cy.wait("@leaves").its("response.statusCode").should("eq", 200);
+  }
+}
+
+export default new loginPage();
